2fd003526cf72ed2dac096acdffb63d7e23f4011
Kedir Biruh <kedirbiruh1114@gmail.com>
2025-07-30 10:26:47 +0200

    Überschrifft ersetzt

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-var variable = 10;
-console.log(variable);
-alert(variable);
+let heute = new Date();
+let tag = heute.getDate();
+let monat = heute.getMonth();
+let jahr = heute.getFullYear();
+let wochentagIndex = heute.getDay();
+
+
+
+document.getElementById("heute").textContent = tag;
+
+let volleDatum = tag + "." + (monat + 1) + "." + jahr;
+document.getElementById("vollesDatum").textContent = volleDatum;
+
